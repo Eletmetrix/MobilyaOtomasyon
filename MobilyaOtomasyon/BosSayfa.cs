277ee69768f64d90a1f6faa8b7be4342d285ce31
Eletmetrix <erdemerdemmc@gmail.com>
2023-01-17 20:07:29 +0300

    müşteri listesi ve müşteri düzenleme eklendi

---
+++ MobilyaOtomasyon/BosSayfa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MobilyaOtomasyon
+{
+    public partial class BosSayfa : Form
+    {
+        protected AnaForm UstForm = null;
+
+        public BosSayfa()
+        {
+            InitializeComponent();
+        }
+
+        public BosSayfa(AnaForm Parent)
+        {
+            InitializeComponent();
+
+            UstForm = Parent;
+        }
+
+        public virtual void SayfaAcildi()
+        {
+            // Burası, sayfanın açılmasında çalışacak.
+        }
+
+        public virtual void SayfaKapandi()
+        {
+            // Burası, sayfanın kapanmasında çalışacak.
+        }
+    }
+}
